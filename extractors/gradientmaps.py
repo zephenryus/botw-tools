ade87af6bd8b5485f4552e0f06da03731777a38d
Kevin Jensen <kevin@turtlerockweb.com>
2017-11-27 17:00:08 -0700

    extracted the gradient maps to a separate file, cleaned up some of the height map parser

---
+++ extractors/gradientmaps.py
@@ -0,0 +1,16 @@
+Terrain = [
+    {"stop": 0.00, "color": {'r': 0x00, 'g': 0x9a, 'b': 0x24}},
+    {"stop": 0.45, "color": {'r': 0xff, 'g': 0xff, 'b': 0x00}},
+    {"stop": 0.90, "color": {'r': 0xff, 'g': 0x00, 'b': 0x00}},
+    {"stop": 1.00, "color": {'r': 0xff, 'g': 0xe0, 'b': 0xe0}}
+]
+
+Water = [
+    {"stop": 0.00, "color": {'r': 0x34, 'g': 0x40, 'b': 0x44}},
+    {"stop": 1.00, "color": {'r': 0x15, 'g': 0x54, 'b': 0xd1}}
+]
+
+WaterDepth = [
+    {"stop": 0.00, "color": {'r': 0x15, 'g': 0x54, 'b': 0xd1}},
+    {"stop": 1.00, "color": {'r': 0x34, 'g': 0x40, 'b': 0x44}}
+]
